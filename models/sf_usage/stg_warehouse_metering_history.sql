07c891796472c2720d9e4feb14afbd7f23f7620e
phongchau@gmail.com <phongchau@gmail.com>
2025-10-27 08:07:57 +0000

    updated

---
+++ models/sf_usage/stg_warehouse_metering_history.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+select
+    warehouse_id,
+    warehouse_name,
+    start_time::timestamp_ntz as start_time,
+    end_time::timestamp_ntz as end_time,
+    credits_used,
+    credits_used_compute,
+    credits_used_cloud_services
+from snowflake.account_usage.warehouse_metering_history
+where start_time >= dateadd('day', -90, current_timestamp())
